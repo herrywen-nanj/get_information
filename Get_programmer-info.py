190f4a23de7ed0c50f2894483f4cc2bb306f4ca6
root <root@localhost.localdomain>
2019-11-25 06:59:44 -0500

    解决format出现keyerror

--- Get_programmer-info.py
+++ Get_programmer-info.py
@@ -33,7 +33,7 @@ def filterList():
 
 # 使用format格式化，出现$NF无法识别,待解决
 def filterprocess(PID):
-    command = "pwdx {PID}".format(PID)  + "|" + "awk '{print $NF}'"
+    command = "pwdx {PID}".format(PID=PID)  + "|" + "awk '{print $NF}'"
     workdir = getComStr(command)
     return workdir
 
